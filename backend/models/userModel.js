ce1d721d48056bb470447b5e09f923bb214cab9b
Daniyal Anjum <d.anjum786@gmail.com>
2024-05-14 06:42:51 +0500

    userSchema complete

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0, // 0 means user
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
