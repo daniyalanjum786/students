41552d5c0b121f2731a54e94a3185bdd6c8dd01e
Daniyal Anjum <d.anjum786@gmail.com>
2024-05-15 11:04:29 +0500

    user login with jwt token complete

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
-import { passwordEncrypt } from "../helpers/userHelper.js";
+import { passwordEncrypt, passwordCompare } from "../helpers/userHelper.js";
 import { uploadImageOnCloudinary } from "../helpers/cloudinaryHelper.js";
+import jwt from "jsonwebtoken";
 
 const registerController = async (req, res) => {
   try {
@@ -10,7 +11,9 @@ const registerController = async (req, res) => {
     console.log(picture);
 
     if (!name || !email || !password || !picture) {
-      return res.status(400).json({ message: "Please fill all the fields" });
+      return res
+        .status(400)
+        .send({ success: false, message: "Please fill all the fields" });
     }
     // Checking if user email already present in the database or not
     const isExist = await userModel.findOne({ email });
@@ -60,4 +63,53 @@ const registerController = async (req, res) => {
   }
 };
 
-export { registerController };
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Please fill all the fields" });
+    }
+    // check existing user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .send({ success: false, message: "Email does not exists" });
+    }
+    // matching password
+    const matchPassword = await passwordCompare(password, user.password);
+    if (!matchPassword) {
+      return res
+        .status(401)
+        .send({ success: false, message: "Incorrect Email/Password" });
+    }
+    // assigning token to a user
+    const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXP,
+    });
+    const tokenOptions = {
+      httpOnly: true,
+      secure: true,
+    };
+    user.password = undefined;
+    // Set token in cookies
+    return res.cookie("token", token, tokenOptions).status(200).send({
+      success: true,
+      message: "Login Successful",
+      user,
+      token: token,
+    });
+  } catch (error) {
+    console.log(`registerController Error - ${error}`);
+    res.status(500).send({
+      success: false,
+      message: "Error in registerController",
+      error,
+    });
+  }
+};
+
+export { registerController, loginController };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { registerController } from "../controllers/userControllers.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/userControllers.js";
 import { upload } from "../middlewares/multerMiddleware.js";
 
 const router = express.Router();
@@ -7,5 +10,6 @@ const router = express.Router();
 //localhost:8000/api/v1/user/register
 
 router.post("/register", upload.single("picture"), registerController);
+router.post("/login", loginController);
 
 export default router;
