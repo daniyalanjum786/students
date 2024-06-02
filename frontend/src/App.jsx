602d9e2d602f866bd895c16780842697c767a237
Daniyal Anjum <d.anjum786@gmail.com>
2024-06-02 11:29:48 +0500

    pages and navigation menu add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,17 @@ import Dashboard from "./pages/Admin/Dashboard";
 import { ToastContainer } from "react-toastify";
 import HomePage from "./pages/HomePage";
 import AddProduct from "./pages/Admin/AddProduct";
+import Navbar from "./components/Navbar";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/admin" element={<DashboardLayout />}>
             <Route index element={<Dashboard />} />
@@ -23,6 +29,10 @@ function App() {
             <Route path="orders" element={<Orders />} />
           </Route>
           <Route path="/" element={<HomePage />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,174 @@
+import { CircleUser, Menu, Package2 } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { logout } from "@/store/features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
+function Navbar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.auth.user);
+  const handleLogout = () => {
+    dispatch(logout())
+      .unwrap()
+      .then((response) => {
+        if (response?.success == true) {
+          toast.success(response.message, { autoClose: 2000 });
+          navigate("/login");
+        } else {
+          toast.error(response.message, { autoClose: 2000 });
+        }
+      })
+      .catch((error) => {
+        toast.error(error, { autoClose: 2000 });
+      });
+  };
+  return (
+    <>
+      <header className="sticky shadow top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <Link
+            to="/"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+          >
+            <Package2 className="h-6 w-6" />
+            <p>
+              {" "}
+              Shop<span className="text-orange-400">Wave</span>
+            </p>
+            <span className="sr-only">Acme Inc</span>
+          </Link>
+          <Link
+            to="/"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Home
+          </Link>
+          <Link
+            to="/shop"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Shop
+          </Link>
+          <Link
+            to="/about"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Contact
+          </Link>
+          <Link
+            to="/cart"
+            className="text-muted-foreground transition-colors hover:text-foreground"
+          >
+            Cart (0)
+          </Link>
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <nav className="grid gap-6 text-lg font-medium">
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
+                <Package2 className="h-6 w-6" />
+                <span className="sr-only">Acme Inc</span>
+              </Link>
+              <Link
+                href="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Orders
+              </Link>
+              <Link
+                href="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Products
+              </Link>
+              <Link
+                href="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
+                Customers
+              </Link>
+              <Link href="#" className="hover:text-foreground">
+                Settings
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <div className="ml-auto">
+          {user == null ? (
+            <div>
+              <Button variant="outline" className="mr-3">
+                <Link to="/login">Login</Link>
+              </Button>
+              <Button className="bg-black">
+                <Link to="/register">Register</Link>
+              </Button>
+            </div>
+          ) : (
+            <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="secondary"
+                    size="icon"
+                    className="rounded-full"
+                  >
+                    <CircleUser className="h-5 w-5" />
+                    <span className="sr-only">Toggle user menu</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Settings</DropdownMenuItem>
+                  <DropdownMenuItem>Support</DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <button onClick={handleLogout}>Logout</button>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          )}
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
